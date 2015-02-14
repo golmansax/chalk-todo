08beb33b31595353644adf5cbb5e8c601412cad8
Shirley Shaw <shirleyshaw.is@gmail.com>
2015-02-13 17:08:23 -0800

    testing

--- app/assets/javascripts/components/todo_list_container.js.jsx
+++ app/assets/javascripts/components/todo_list_container.js.jsx
@@ -7,19 +7,35 @@ Chk.TodoListContainer = (function () {
       todos: React.PropTypes.array.isRequired
     },
 
+    filterList: function(event){
+      if (event.target.checked){
+        var updatedList= this.props.todos 
+        _.filter(updatedList, function(todo){return todo.complete_date})
+
+      this.setState({todos:updatedList});
+      }
+    },
+
+    getInitialState: function(){
+      return{
+        todos: this.props.todos
+      }
+    },
+
+
     render: function () {
       return (
         <div>
           <h4>
             <div className='row checkbox'>
               <label>
-                <input type='checkbox' defaultChecked={true} />
+                <input type='checkbox' defaultChecked={true} onChange={this.filterList}/>
                 Show completed
               </label>
             </div>
           </h4>
           <br />
-          <Chk.TodoList {...this.props} todos={this.props.todos} />
+          <Chk.TodoList {...this.props} todos={this.state.todos} />
         </div>
       );
     }
