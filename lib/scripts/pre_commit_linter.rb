f9f3631017c45c69ce3f0bc34e0b115f5e4819b9
Ivan Stroganov <knowhere@gmail.com>
2015-03-20 09:22:52 -0700

    Pre-commit linter - make rubocop happy
    also add method for rubocop linting

--- lib/scripts/pre_commit_linter.rb
+++ lib/scripts/pre_commit_linter.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/ruby ruby
 
 # This script looks for files in staged changes for
 # files in stylesheets/ or javascripts dir, and runs
@@ -14,40 +14,57 @@
 # To install - symlink it into hooks directory like this:
 # ln -s ../../lib/scripts/pre-commit-linter.rb .git/hooks/pre-commit
 
-def run_csslint file
-  puts "running 'bundle exec scss-lint #{file}'"
-  result = system 'bundle', 'exec', 'scss-lint', file
+def run_rubocop(file)
+  puts "running 'bundle exec rubocop -D #{file}'"
+  result = system 'bundle', 'exec', 'rubocop', '-D', file
   if result.nil?
-    puts "Couldn't run the command: error was #{$?}"
+    puts "Couldn't run the command: error was #{$CHILD_STATUS}"
   # non-zero outcome of the command yields a "false", so exit
   elsif !result
     exit(1)
   end
 end
 
-def run_jscs file
+def run_csslint(file)
+  puts "running 'bundle exec scss-lint #{file}'"
+  result = system 'bundle', 'exec', 'scss-lint', file
+  if result.nil?
+    puts "Couldn't run the command: error was #{$CHILD_STATUS}"
+  # non-zero outcome of the command yields a "false", so exit
+  elsif !result
+    exit(1)
+  end
+end
+
+def run_jscs(file)
   puts "running 'jscs --esprima=esprima-fb #{file}'"
   result = system 'jscs', '--esprima=esprima-fb', file
   if result.nil?
-    puts "Couldn't run the command: error was #{$?}"
+    puts "Couldn't run the command: error was #{$CHILD_STATUS}"
   elsif !result
     exit(1)
   end
 end
 
-def run_jsxhint file
+def run_jsxhint(file)
   puts "running 'jsxhint --verbose #{file}'"
   result = system 'jsxhint', '--verbose', file
   if result.nil?
-    puts "Couldn't run the command: error was #{$?}"
+    puts "Couldn't run the command: error was #{$CHILD_STATUS}"
   elsif !result
     exit(1)
   end
 end
 
+RB_FILES = `git diff --cached --name-only --diff-filter=AM | grep -E "(\.rb)"`
+JS_FILES = `git diff --cached --name-only --diff-filter=AM | grep -E "(/javascripts/)"`
+CSS_FILES = `git diff --cached --name-only --diff-filter=AM | grep -E "(/stylesheets/)"`
 
-JS_FILES=`git diff --cached --name-only | grep -E "(/javascripts/)"`
-CSS_FILES=`git diff --cached --name-only | grep -E "(/stylesheets/)"`
+if RB_FILES.length > 0
+  RB_FILES.split("\n").each do |file|
+    run_rubocop(file)
+  end
+end
 
 if CSS_FILES.length > 0
   CSS_FILES.split("\n").each do |file|
